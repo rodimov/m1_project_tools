90ec3337945b380cf2a60b32e19afaf303608ae3
Artem Rodimov <artyomrodimov@yandex.ru>
2022-05-02 06:40:16 +0300

    feat: added csv export

---
+++ CSVExporter.py
@@ -0,0 +1,47 @@
+import sqlite3 as sql
+import os
+import csv
+
+
+def export_to_csv(db_path, csv_path):
+    conn = sql.connect(db_path)
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM requests")
+
+    with open(csv_path, "w", newline='', encoding='utf-8') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow([i[0] for i in cursor.description])
+
+        data = cursor.fetchone()
+
+        while data is not None and len(data):
+            csv_writer.writerow(data)
+            data = cursor.fetchone()
+
+        print(f'Exported: {db_path}')
+
+
+def get_all_dir(path):
+    return [name for name in os.listdir(path) if os.path.isdir(path + name)]
+
+
+def get_all_db(path):
+    return [name for name in os.listdir(path) if name.endswith(".db")]
+
+
+def export_dir(path):
+    servers_dirs = get_all_dir(path)
+
+    for server_dir in servers_dirs:
+        csv_path = path + server_dir + "/csv/"
+        db_path = path + server_dir + "/db/"
+
+        if not os.path.exists(csv_path):
+            os.makedirs(csv_path)
+
+        db_files = get_all_db(db_path)
+
+        for db_file in db_files:
+            csv_filename = os.path.splitext(db_file)[0] + '.csv'
+            export_to_csv(db_path + db_file, csv_path + csv_filename)
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from LogDownloader import LogDownloader
 from LogDownloader import Server
 
 import json
+import sys
+import CSVExporter as exporter
 
 
-if __name__ == '__main__':
+def run_download():
     servers = []
 
     with open('servers.json') as servers_file:
@@ -15,4 +17,35 @@ if __name__ == '__main__':
 
         logDownloader = LogDownloader(servers)
         logDownloader.download()
+
+
+def convert_sqlite_to_csv():
+    print('Select directory:')
+
+    directories = exporter.get_all_dir('./')
+
+    for i in range(len(directories)):
+        print(f'{i}) {directories[i]}')
+
+    dir_index = int(input())
+
+    exporter.export_dir('./' + directories[dir_index] + '/')
+
+
+if __name__ == '__main__':
+    while True:
+
+        print('Select option:')
+        print('1) Download logs')
+        print('2) Convert sqlite to csv')
+        print('0) Exit')
+
+        option = input()
+
+        if option == '0':
+            sys.exit(0)
+        elif option == '1':
+            run_download()
+        elif option == '2':
+            convert_sqlite_to_csv()
 
