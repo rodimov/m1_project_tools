f8a5c9a029d69a8f4ecff3d9131cb1c74a095440
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-03 11:23:33 +0000

    Adding tarfile member sanitization to extractall()

--- Extractor.py
+++ Extractor.py
@@ -12,7 +12,26 @@ def extract_file(archive_path, extract_to):
         db_members = [tarinfo for tarinfo in tar.getmembers()
                       if tarinfo.name.startswith("./db/")]
 
-        tar.extractall(extract_to, db_members)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner) 
+            
+        
+        safe_extract(tar, extract_to, db_members)
         print(f'file {archive_path} extracted!')
 
     journal_files = [name for name in os.listdir(extract_to + 'db/') if name.endswith('.db-journal')]
